987bf17734da81dc192b010509e170cd0e954ad2
ewcgit <76733558+ewcgit@users.noreply.github.com>
2023-08-13 21:52:56 -0400

    Add files via upload

---
+++ demo.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 14, 2023 at 03:52 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `measurement`
+--
+
+CREATE TABLE `measurement` (
+  `measurementID` int(11) NOT NULL,
+  `value` float DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `sensorID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `measurement`
+--
+
+INSERT INTO `measurement` (`measurementID`, `value`, `timestamp`, `sensorID`) VALUES
+(1, 40, '2023-08-04 18:30:00', 1),
+(2, 78, '2023-08-04 18:30:00', 2),
+(3, 80000, '2023-08-04 18:30:00', 3),
+(4, 46, '2023-08-04 18:30:00', 1),
+(5, 98, '2023-08-04 18:30:00', 2),
+(6, 60000, '2023-08-04 18:30:00', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sensor`
+--
+
+CREATE TABLE `sensor` (
+  `SensorID` int(11) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sensor`
+--
+
+INSERT INTO `sensor` (`SensorID`, `type`, `description`) VALUES
+(1, 'WATER', 'Reads soil water levels'),
+(2, 'TEMPERATURE', 'Reads soil temperature levels'),
+(3, 'LIGHT', 'Reads light levels');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`) VALUES
+(1, 'tester', 'tester'),
+(3, 'tom', 'tommy'),
+(4, 'toms', 'toms');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `measurement`
+--
+ALTER TABLE `measurement`
+  ADD PRIMARY KEY (`measurementID`),
+  ADD KEY `sensorID` (`sensorID`);
+
+--
+-- Indexes for table `sensor`
+--
+ALTER TABLE `sensor`
+  ADD PRIMARY KEY (`SensorID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `measurement`
+--
+ALTER TABLE `measurement`
+  MODIFY `measurementID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `sensor`
+--
+ALTER TABLE `sensor`
+  MODIFY `SensorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `measurement`
+--
+ALTER TABLE `measurement`
+  ADD CONSTRAINT `measurement_ibfk_1` FOREIGN KEY (`sensorID`) REFERENCES `sensor` (`SensorID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
